ff38c56648add50953f8aa35121bbc1d5f8d15b1
Luis Gustavo Araujo Markovic <luisgustav743@gmail.com>
2026-03-12 11:15:20 -0300

    Removing a file that is no longer in use.

--- test/waiters_test.py
+++
@@ -1,9 +0,0 @@
-# Waiter for testing
-
-{
-    "name": "João Silva",
-    "cpf": "123.456.789-09",
-    "wage": 1500.0,
-    "email": "joao.silva@email.com",
-    "phone_number": "(11) 99999-9999"
-}
